c95e03d937bca05fd0c1c86df4f9771144736396
RiyanSinaga007 <45660151+RiyanSinaga007@users.noreply.github.com>
2018-12-24 20:43:04 +0700

    Add files via upload

---
+++ Program Memmvalidai pembagian Agar Tidak Nol.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int a,b,hasil;
+	
+	cout<<"PROGRAM VALIDASI PEMBAGIAN TIDAK SAMA DENGAN NOL"<<endl;
+	cout<<"masukan bilangan yang akan di bagi : ";
+	cin>>a;
+	cout<<"masukan bilangan pembagi : ";
+	cin>>b;
+	if(b==0)
+	{
+		cout<<"bilangan pembagi tidak boleh nol"<<endl;
+	}
+	else{
+		hasil=a/b;
+		cout<<"hasilnya adalah : "<<a/b<<endl;}
+		return 0;
+		
+}
